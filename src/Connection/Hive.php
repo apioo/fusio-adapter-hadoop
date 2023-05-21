b18b93b306badf387889797b692a53271b145c74
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-05-21 10:38:13 +0200

    extend connection abstract

--- src/Connection/Hive.php
+++ src/Connection/Hive.php
@@ -21,7 +21,7 @@
 
 namespace Fusio\Adapter\Hadoop\Connection;
 
-use Fusio\Engine\ConnectionInterface;
+use Fusio\Engine\ConnectionAbstract;
 use Fusio\Engine\Form\BuilderInterface;
 use Fusio\Engine\Form\ElementFactoryInterface;
 use Fusio\Engine\ParametersInterface;
@@ -33,7 +33,7 @@ use Fusio\Engine\ParametersInterface;
  * @license http://www.gnu.org/licenses/agpl-3.0
  * @link    https://www.fusio-project.org/
  */
-class Hive implements ConnectionInterface
+class Hive extends ConnectionAbstract
 {
     public function getName(): string
     {
